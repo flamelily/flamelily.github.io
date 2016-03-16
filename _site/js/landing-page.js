16da1be0d095aa97f6f1d906e384eac724b97568
flamelily <info@flamelily.co.uk>
2016-03-16 14:17:12 +0000

    Comment out legacy code in js

--- _site/js/landing-page.js
+++ _site/js/landing-page.js
@@ -14,18 +14,18 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+// Closes the Responsive Menu on Menu Item Click - legacy code that we removed
+// $('.navbar-collapse ul li a').click(function() {
+//     $('.navbar-toggle:visible').click();
+// });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
+// $('div.modal').on('show.bs.modal', function() {
+// 	var modal = this;
+// 	var hash = modal.id;
+// 	window.location.hash = hash;
+// 	window.onhashchange = function() {
+// 		if (!location.hash){
+// 			$(modal).modal('hide');
+// 		}
+// 	}
+// });
